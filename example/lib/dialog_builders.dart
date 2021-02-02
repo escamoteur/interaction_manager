d4df27edd6e645fe8dff68a4b4efa52273c313be
escamoteur <github@burkharts.net>
2021-02-02 16:44:40 +0100

    updated to latest Flutterversion

--- example/lib/dialog_builders.dart
+++ example/lib/dialog_builders.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:interaction_manager/interaction_manager.dart';
 
 void registerDialogs(InteractionManager ia) {
-  ia.registerCustomDialog<Map<String, String>>(
-      buildTestDialog, 'TestDialog');
+  ia.registerCustomDialog<Map<String, String>>(buildTestDialog, 'TestDialog');
 }
 
-Widget buildTestDialog(BuildContext context, Map<String, String> data) => AlertDialog(
+Widget buildTestDialog(BuildContext context, Map<String, String> data) =>
+    AlertDialog(
       title: Text(data['title']),
       content: Text(data['message']),
       actions: [
-        FlatButton(
+        TextButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
